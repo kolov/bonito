e519bf8e08d54a7e44f25cc56b10354f143e4220
Assen Kolov <assen.kolov@gmail.com>
2016-12-09 22:26:17 +0100

    parsing droplet response

--- command/list.go
+++ command/list.go
@@ -8,10 +8,40 @@ import (
 	//	"net/url"
 	"github.com/codegangsta/cli"
 	//	"reflect"
+	"os"
 )
 
-type ListResponse struct {
-	Valid bool   `json:"valid"`
+type Droplet struct {
+	id           int      `json:"id"`
+	name         string      `json:"name"`
+	memory       int      `json:"memory"`
+	vcpus        int      `json:"vcpus"`
+	locked       bool      `json:"locked"`
+	status       string      `json:"status"`
+	kernel       struct {
+			     id      int      `json:"id"`
+			     name    string      `json:"name"`
+			     version string      `json:"version"`
+		     } `json:"kernel"`
+	created_at   string      `json:"created_at"`
+	backup_ids   []string      `json:"backup_ids"`
+	snapshot_ids []string      `json:"snapshot_ids"`
+	image        struct {
+			     id             int      `json:"id"`
+			     name           int      `json:"name"`
+			     distribution   int      `json:"distribution"`
+			     slug           int      `json:"slug"`
+			     public         bool      `json:"public"`
+			     regions        []string      `json:"regions"`
+			     created_at     string      `json:"created_at"`
+			     min_disk_size  int      `json:"min_disk_size"`
+			     itype          string      `json:"type"`
+			     size_gigabytes int      `json:"size_gigabytes"`
+		     }  `json:"image"`
+}
+
+type ListDropletsResponse struct {
+	droplets [] Droplet   `json:"droplets"`
 }
 
 func CmdList(c *cli.Context) {
@@ -19,11 +49,14 @@ func CmdList(c *cli.Context) {
 	url := fmt.Sprintf("https://api.digitalocean.com/v2/droplets?page=1&per_page=100")
 
 	req, err := http.NewRequest("GET", url, nil)
-
 	if err != nil {
 		log.Fatal("NewRequest: ", err)
 		return
 	}
+
+	req.Header.Add("Authorization", "Bearer " + os.Getenv("DO_TOKEN_SARDINE"))
+	req.Header.Add("Content-Type", "application/json")
+
 
 	// For control over HTTP client headers,
 	// redirect policy, and other settings,
@@ -40,18 +73,22 @@ func CmdList(c *cli.Context) {
 		return
 	}
 
+	fmt.Print("Response: ")
+	fmt.Println(resp)
+	fmt.Print("Body: ")
+	fmt.Println(resp.Body)
 	// Callers should close resp.Body
 	// when done reading from it
 	// Defer the closing of the body
 	defer resp.Body.Close()
 
 	// Fill the record with the data from the JSON
-	var record ListResponse
+	var record ListDropletsResponse
 
 	// Use json.Decode for reading streams of JSON data
 	if err := json.NewDecoder(resp.Body).Decode(&record); err != nil {
 		log.Println(err)
 	}
 
-	fmt.Println("Phone No. = ", record.Valid)
+	fmt.Println("record = ", record)
 }
