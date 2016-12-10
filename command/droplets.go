ae0ed157914f5bdebc6efb394c59ca5df235a09a
Assen Kolov <assen.kolov@gmail.com>
2016-12-10 17:48:41 +0100

    common func works in interface{}

--- command/droplets.go
+++ command/droplets.go
@@ -7,13 +7,43 @@ import (
 	"github.com/kolov/sardine/common"
 )
 
-
+type Droplet struct {
+	Id           int      `json:"id"`
+	Name         string      `json:"name"`
+	Memory       int      `json:"memory"`
+	Vcpus        int      `json:"vcpus"`
+	Locked       bool      `json:"locked"`
+	Status       string      `json:"status"`
+	Kernel       struct {
+			     Id      int      `json:"id"`
+			     Name    string      `json:"name"`
+			     Version string      `json:"version"`
+		     } `json:"kernel"`
+	created_at   string      `json:"created_at"`
+	Backup_ids   []int      `json:"backup_ids"`
+	Snapshot_ids []int      `json:"snapshot_ids"`
+	Image        struct {
+			     Id             int      `json:"id"`
+			     Name           string      `json:"name"`
+			     Distribution   string      `json:"distribution"`
+			     Slug           string      `json:"slug"`
+			     Public         bool      `json:"public"`
+			     Regions        []string      `json:"regions"`
+			     Created_at     string      `json:"created_at"`
+			     Min_disk_size  int      `json:"min_disk_size"`
+			     Itype          string      `json:"type"`
+			     Size_gigabytes float32      `json:"size_gigabytes"`
+		     }  `json:"image"`
+}
+type DropletsList struct {
+	Droplets [] Droplet   `json:"droplets"`
+}
 
 func CmdDroplets(c *cli.Context) {
 
 	url := fmt.Sprintf("https://api.digitalocean.com/v2/droplets?page=1&per_page=100")
 
-	var record common.DropletsList
+	var record DropletsList
 
 	common.Query(url, &record)
 
--- common/utils.go
+++ common/utils.go
@@ -8,38 +8,7 @@ import (
 	"encoding/json"
 )
 
-type Droplet struct {
-	Id           int      `json:"id"`
-	Name         string      `json:"name"`
-	Memory       int      `json:"memory"`
-	Vcpus        int      `json:"vcpus"`
-	Locked       bool      `json:"locked"`
-	Status       string      `json:"status"`
-	Kernel       struct {
-			     Id      int      `json:"id"`
-			     Name    string      `json:"name"`
-			     Version string      `json:"version"`
-		     } `json:"kernel"`
-	created_at   string      `json:"created_at"`
-	Backup_ids   []int      `json:"backup_ids"`
-	Snapshot_ids []int      `json:"snapshot_ids"`
-	Image        struct {
-			     Id             int      `json:"id"`
-			     Name           string      `json:"name"`
-			     Distribution   string      `json:"distribution"`
-			     Slug           string      `json:"slug"`
-			     Public         bool      `json:"public"`
-			     Regions        []string      `json:"regions"`
-			     Created_at     string      `json:"created_at"`
-			     Min_disk_size  int      `json:"min_disk_size"`
-			     Itype          string      `json:"type"`
-			     Size_gigabytes float32      `json:"size_gigabytes"`
-		     }  `json:"image"`
-}
-type DropletsList struct {
-	Droplets [] Droplet   `json:"droplets"`
-}
-func Query(url string, result *DropletsList) {
+func Query(url string, result interface{}) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Fatal("NewRequest: ", err)
